09394e96f5ce4b2a064039dfe0eaa200d956824e
pally1995 <51261320+pally1995@users.noreply.github.com>
2023-06-01 22:09:43 +0100

    added carousel

--- app.js
+++ app.js
@@ -9,39 +9,41 @@ burger.addEventListener('click', () => {
     burger.classList.toggle("active");
 })
 
-
-slides.forEach((slide, indx) => {
-  slide.style.transform = `translateX(${indx * 100}%)`;
-});
-
-
-let curSlide = 0;
-
-let maxSlide = slides.length - 1;
-
-
-nextSlide.addEventListener("click", function () {
-
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
-
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${index * 100}%)`;
   });
-});
-
-prevSlide.addEventListener("click", function () {
-
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
+  
+  let curSlide = 0;
+  let maxSlide = slides.length - 1;
+  
+  function goToNextSlide() {
+    if (curSlide === maxSlide) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+  
+    updateSlidePosition();
   }
-
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
+  
+  function goToPrevSlide() {
+    if (curSlide === 0) {
+      curSlide = maxSlide;
+    } else {
+      curSlide--;
+    }
+  
+    updateSlidePosition();
+  }
+  
+  function updateSlidePosition() {
+    slides.forEach((slide, indx) => {
+      slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+  }
+  
+  nextSlide.addEventListener("click", goToNextSlide);
+  prevSlide.addEventListener("click", goToPrevSlide);
+  
+  setInterval(goToNextSlide, 5000); // Automatically go to the next slide every 5 seconds
+  
