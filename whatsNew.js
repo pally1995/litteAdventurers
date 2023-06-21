8cf2a056dfb6f91377bb6ca1d70335b6fa30a17f
pally1995 <51261320+pally1995@users.noreply.github.com>
2023-06-21 21:24:24 +0100

    updated footer
    changed footer so moves down with rest of content

--- whatsNew.js
+++ whatsNew.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector(".burger");
 const navBar = document.querySelector("nav");
 const main = document.querySelector(".main");
+const footer = document.querySelector("footer");
 
 burger.addEventListener('click', () => {
     navBar.classList.toggle("open");
@@ -8,7 +9,9 @@ burger.addEventListener('click', () => {
 
     if(navBar.classList.contains("open")){
       main.style.top = "15em";
+      footer.style.top = "15rem";
     } else {
       main.style.top = "0rem";
+      footer.style.top = "0rem";
     }
 })
